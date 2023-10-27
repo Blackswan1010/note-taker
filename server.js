2c0faff39936457e664d2ac2287ee6f019c233cb
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-27 05:21:48 -0700

    implemented the delete request, add/edit developer comments

--- server.js
+++ server.js
@@ -1,71 +1,76 @@
-// import express module to our server
+// Import express module to our server
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-// import require("./db/db.json")
+
+// Import require("./db/db.json")
 const db = require('./db/db.json');
-// create app variable pointing to new express object(express())
+// Create app variable pointing to new express object(express())
 const app = express();
 
-
 const PORT = process.env.PORT || 3001;
+// Create middleware
 // app.use(*middleware*){// -json, urlencoded, staticify(public)}
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// get/delete/post requests:
-// get request to send index.html, route: '/'
+// GET/POST/DELETE requests:
+// GET request to send index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// get request to send notes.html, route: '/notes'
+// GET request to send notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// get request to fetch our api to send notes from db.json, route: '/api/notes'
+// GET request to fetch our api to send notes from db.json
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        if(err){
+        if (err) {
             console.error(err);
         } else {
             const parsedData = JSON.parse(data);
             res.status(200).json(parsedData);
         }
     })
-    
+
 });
 
-// post request to fetch our api to modify with parsed req.body data and push post to db.json with fs.writeFile, route: '/api/notes', return res.json;
+// POST request to fetch our api to modify with parsed req.body data and push post to db.json with fs.writeFile, route: '/api/notes', return res.json;
 app.post('/api/notes', (req, res) => {
 
-    const {title, text} = req.body;
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
 
-    if(title && text){
+    // If title and text input exists then create and save the new note
+    if (title && text) {
         const newNote = {
             title,
             text,
-            note_id: uuidv4(),
-          };
+            id: uuidv4(),
+        };
 
-          fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if(err){
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
                 console.error(err);
             } else {
+                // Converting data into a JSON object
                 const parsedData = JSON.parse(data);
-
+                // Pushing the newNote object into the parsedData JSON object
                 parsedData.push(newNote);
 
                 db.push(newNote);
 
-                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4),
-                 (writeErr) => writeErr ? console.error(writeErr) : console.info('Successfully updated note!'));
+                // Writing updated notes back to db.json
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (writeErr) =>
+                    writeErr ? console.error(writeErr) : console.info('Successfully updated note!'));
             }
-          })
-    
+        })
+
         const response = {
             status: 'success',
             body: newNote
@@ -78,9 +83,23 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
-// delete request to delete specific note, *EXTRA CRED* remove specific data from db.json and push with fs.writeFile,  route: '/api/notes/:notes_id', return res.json;
+
+// DELETE request to delete specific note and specific data from db.json and push with fs.writeFile
 app.delete('/api/notes/:id', (req, res) => {
 
+    for (let i = 0; i < db.length; i++) {
+        let note = db[i];
+
+        if (note.id == req.params.id) {
+            db.splice(i, 1);
+            fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(db, null, 2));
+
+            break;
+        }
+    }
+
+    console.info('Note deleted!');
+    res.json('Note deleted!');
 });
 
 // app.listen
